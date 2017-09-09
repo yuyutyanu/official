a13bca30c0069742c7e06d0676188a9faa3e165b
uufish <uufish@sankaku.io>
2017-09-10 01:23:22 +0900

    update : サンプルデータの追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,52 +1,67 @@
 <template>
-  <section class="container">
+  <section class='container'>
     <div>
-      <logo />
-      <h1 class="title">
-        pixelgram.now.sh
+      <h1 class='title'>
+        Pixelgram
       </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
+      <h2 class='subtitle'>
+        Nuxt Application
       </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
+      <div class='links'>
+        <a href='https://medium.com/websocket' target='_blank' class='button--green'>Medium</a>
+        <a href='https://github.com/pixelgram-jp' target='_blank' class='button--grey'>GitHub</a>
+      </div>
+      <div class='newsRoot'>
+        <div class='news' v-for='news in allNews'>
+          <div class='newsDate'>
+            {{news.dateYYYYMMDD}}
+          </div>
+          <div class='newsContent' v-text='news.content' />
+        </div>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-  import Logo from '~/components/Logo.vue'
+  import { mapMutations } from 'vuex'
 
   export default {
-    components: {
-      Logo
+    computed: {
+      allNews () {
+        return this.$store.state.graphcms.allNews
+      }
+    },
+    methods: {
+      ...mapMutations({
+        queryAllNews: 'graphcms/queryAllNews'
+      })
+    },
+    created () {
+      this.$store.dispatch('graphcms/queryAllNews')
     }
   }
 </script>
 
 <style lang='stylus'>
   .container {
-    min-height: 100vh
-    display: flex
-    justify-content: center
-    align-items: center
-    text-align: center
+    padding-top 100px
+    padding-left 40px
   }
 
   .title {
-    font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif /* 1 */
+    font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif /* 1 */
     display: block
     font-weight: 300
-    font-size: 100px
+    font-size: 50px
     color: #35495e
     letter-spacing: 1px
   }
 
   .subtitle {
+    padding-top 5px
     font-weight: 300
-    font-size: 42px
+    font-size: 20px
     color: #526488
     word-spacing: 5px
     padding-bottom: 15px
@@ -54,5 +69,53 @@
 
   .links {
     padding-top: 15px
+  }
+
+  .newsRoot {
+    padding-top: 10px
+  }
+
+  .news {
+    padding-top: 20px
+  }
+
+  .newsDate {
+    font-size: 16px
+  }
+
+  .newsContent {
+    padding-top 5px
+    padding-left 2px
+    font-size: 18px
+    white-space: pre
+  }
+
+  .button--green {
+    display: inline-block
+    border-radius: 4px
+    border: 1px solid #3b8070
+    color: #3b8070
+    text-decoration: none
+    padding: 10px 30px
+  }
+
+  .button--green:hover {
+    color: #fff
+    background-color: #3b8070
+  }
+
+  .button--grey {
+    display: inline-block
+    border-radius: 4px
+    border: 1px solid #35495e
+    color: #35495e
+    text-decoration: none
+    padding: 10px 30px
+    margin-left: 15px
+  }
+
+  .button--grey:hover {
+    color: #fff
+    background-color: #35495e
   }
 </style>
