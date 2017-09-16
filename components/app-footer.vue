90c06511ed27484c52d2c27201c73371291e6984
uufish <uufish@sankaku.io>
2017-09-16 22:52:51 +0900

    add : components/app-footer

---
+++ components/app-footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class='root'>
+    <p class='footer'>
+      © 2017 pixelgram All rights reserved.
+    </p>
+  </div>
+</template>
+
+<style lang='stylus' scoped>
+  .root {
+    width 100%
+    padding-top 80px
+  }
+
+  p.footer {
+    text-align center
+  }
+</style>
