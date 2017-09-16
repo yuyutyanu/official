0f64d3f7b088e40af4d7d560c4259ec41e051674
uufish <uufish@sankaku.io>
2017-09-16 22:55:27 +0900

    update : layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,25 +1,46 @@
 <template>
   <div>
+    <app-menu />
     <nuxt />
+    <app-footer />
   </div>
 </template>
 
+<script>
+  import appFooter from '~/components/app-footer'
+  import appMenu from '~/components/app-menu'
+
+  export default {
+    components: {
+      'app-menu': appMenu,
+      'app-footer': appFooter
+    },
+    head () {
+      return {
+        link: [
+          {rel: 'stylesheet', href: '//fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600|Roboto Mono'}
+        ]
+      }
+    }
+  }
+</script>
+
 <style lang='stylus'>
   html {
-    font-family: 'Source Sans Pro', 'Helvetica Neue', Arial, sans-serif
-    font-size: 15px
-    -ms-text-size-adjust: 100%
-    -webkit-text-size-adjust: 100%
-    -webkit-font-smoothing: antialiased
-    -moz-osx-font-smoothing: grayscale
-    -webkit-font-smoothing: antialiased
-    color: #34495e
-    word-spacing: 1px
-    box-sizing: border-box
+    font-family 'Source Sans Pro', 'Helvetica Neue', Arial, sans-serif
+    font-size 15px
+    -ms-text-size-adjust 100%
+    -webkit-text-size-adjust 100%
+    -webkit-font-smoothing antialiased
+    -moz-osx-font-smoothing grayscale
+    -webkit-font-smoothing antialiased
+    color #0f2350
+    word-spacing 1.5px
+    box-sizing border-box
   }
 
-  *, *:before, *:after {
-    box-sizing: border-box
-    margin: 0
+  *, *before, *after {
+    box-sizing border-box
+    margin 0
   }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,21 @@
 <template>
-  <section class='container'>
-    <div>
-      <h1 class='title'>
-        Pixelgram
-      </h1>
-      <h2 class='subtitle'>
-        Nuxt Application
-      </h2>
-      <div class='links'>
-        <a href='https://medium.com/websocket' target='_blank' class='button--green'>Medium</a>
-        <a href='https://github.com/pixelgram-jp' target='_blank' class='button--grey'>GitHub</a>
-      </div>
-      <div class='newsRoot'>
-        <div class='news' v-for='news in allNews'>
-          <div class='newsDate'>
-            {{news.dateYYYYMMDD}}
-          </div>
-          <div class='newsContent' v-text='news.title' />
-          <div class='newsContent' v-text='news.content' />
-        </div>
-      </div>
-    </div>
-  </section>
+  <div class='root'>
+    <app-title title='pixelgram' />
+    <img class='logo' src='/logo.png' />
+    <app-news :src='allNews' />
+  </div>
 </template>
 
 <script>
   import { mapMutations } from 'vuex'
+  import appTitle from '~/components/app-title'
+  import appNews from '~/components/app-news'
 
   export default {
+    components: {
+      'app-title': appTitle,
+      'app-news': appNews
+    },
     computed: {
       allNews () {
         return this.$store.state.scaphold.allNews
@@ -38,85 +26,39 @@
         queryAllNews: 'scaphold/queryAllNews'
       })
     },
-    fetch ({ store, params }) {
-      return store.dispatch('scaphold/queryAllNews')
+    head () {
+      return {
+        title: 'pixelgram',
+        meta: [{
+          hid: 'description',
+          name: 'description',
+          content: 'no description'
+        }]
+      }
+    },
+    fetch ({store, params}) {
+      if (!store.state.scaphold.allNews) {
+        return store.dispatch('scaphold/queryAllNews')
+      }
     }
   }
 </script>
 
-<style lang='stylus'>
-  .container {
-    padding-top 100px
-    padding-left 40px
-  }
-
+<style lang='stylus' scoped>
   .title {
-    font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif /* 1 */
-    display: block
-    font-weight: 300
-    font-size: 50px
-    color: #35495e
-    letter-spacing: 1px
+    padding-top 40px
   }
 
-  .subtitle {
-    padding-top 5px
-    font-weight: 300
-    font-size: 20px
-    color: #526488
-    word-spacing: 5px
-    padding-bottom: 15px
+  .text-title {
+    font-weight 300
+    margin 0
+    font-size 3.2em
+    text-align center
   }
 
-  .links {
-    padding-top: 15px
-  }
-
-  .newsRoot {
-    padding-top: 10px
-  }
-
-  .news {
-    padding-top: 20px
-  }
-
-  .newsDate {
-    font-size: 16px
-  }
-
-  .newsContent {
-    padding-top 5px
-    padding-left 2px
-    font-size: 18px
-    white-space: pre
-  }
-
-  .button--green {
-    display: inline-block
-    border-radius: 4px
-    border: 1px solid #3b8070
-    color: #3b8070
-    text-decoration: none
-    padding: 10px 30px
-  }
-
-  .button--green:hover {
-    color: #fff
-    background-color: #3b8070
-  }
-
-  .button--grey {
-    display: inline-block
-    border-radius: 4px
-    border: 1px solid #35495e
-    color: #35495e
-    text-decoration: none
-    padding: 10px 30px
-    margin-left: 15px
-  }
-
-  .button--grey:hover {
-    color: #fff
-    background-color: #35495e
+  .logo {
+    display block
+    width 140px
+    margin 40px auto 0
   }
 </style>
