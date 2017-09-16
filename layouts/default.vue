d9bff9698424a5b5dd393bf906c9494b38073afd
uufish <uufish@sankaku.io>
2017-09-16 18:13:01 +0900

    update : layout/default

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,13 +6,15 @@
 
 <style lang='stylus'>
   html {
-    font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif
-    font-size: 16px
-    word-spacing: 1px
+    font-family: 'Source Sans Pro', 'Helvetica Neue', Arial, sans-serif
+    font-size: 15px
     -ms-text-size-adjust: 100%
     -webkit-text-size-adjust: 100%
+    -webkit-font-smoothing: antialiased
     -moz-osx-font-smoothing: grayscale
     -webkit-font-smoothing: antialiased
+    color: #34495e
+    word-spacing: 1px
     box-sizing: border-box
   }
 
