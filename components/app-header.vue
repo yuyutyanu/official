1c34d60ad12363ac69a1b0208f2dbf76f26e854a
uufish <uufish@sankaku.io>
2017-09-21 03:14:25 +0900

    add : components/app-header

---
+++ components/app-header.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class='root'>
+    <img class='logo' src='/logo.png' />
+    <a class='title'>
+      Pixelgram {{page}}
+    </a>
+  </div>
+</template>
+
+<script>
+  export default {
+    computed: {
+      page: function () {
+        if (this.$nuxt.$route.name === 'index') return ''
+        return '/ ' + this.$nuxt.$route.name
+      }
+    }
+  }
+</script>
+
+<style lang='stylus' scoped>
+  .root {
+    padding-top 20px
+    padding-left 20px
+    width 100%
+    z-index 120
+  }
+
+  // md down
+  @media screen and (max-width: 959.99px) {
+    .root {
+      position fixed
+      top 0
+      left 0
+      width 100%
+      height 60px
+      background rgba(245, 245, 245, 0.98)
+    }
+  }
+
+  .logo {
+    display inline-block
+    vertical-align middle
+    width 40px
+  }
+
+  .title {
+    display inline-block
+    vertical-align middle
+    padding-left 10px
+    font-size 1.5rem
+  }
+</style>
