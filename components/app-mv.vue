ec9973088df0d29c84db2a58c14349089260af69
in-the-box <hacoco345@gmail.com>
2018-01-04 00:24:48 +0900

    fix:

--- components/app-mv.vue
+++ components/app-mv.vue
@@ -1,11 +1,47 @@
 <template>
   <div class="root">
-    <img class="mv" src="/mv.jpg" alt="ダミー">
+    <div class="moon"></div>
+    <div class="copy"></div>
   </div>
 </template>
 
 <style scoped>
-  .mv {
-    width: 100%;
+  .root {
+    position: relative;
+    width: 100vw;
+    height: 100vh;
+    background: url('/town.svg') repeat-x bottom;
+    background-size: 1200px;
+  }
+  .moon {
+    background: url('/tuki_mv.svg') no-repeat;
+    width: 74px;
+    height: 72px;
+    position: absolute;
+    top: 75px;
+    left: 50px;
+  }
+  .copy {
+    background: url('/txt_mv.svg') no-repeat;
+    width: 500px;
+    height: 22px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-size: contain;
+  }
+  @media screen and (max-width: 600px) {
+    .root {
+      background-size: contain;
+    }
+    .moon {
+      top: 80px;
+      left: 20px;
+    }
+    .copy {
+      width: 320px;
+      height: 15px;
+    }
   }
 </style>
